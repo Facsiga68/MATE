a76768751681f0182521948dcbc0838b65b0b8b6
BigRicsoo <big.ricsoo@gmail.com>
2021-03-02 15:47:58 +0100

    2021.03.02. 15:47

--- phonebook/src/App.tsx
+++ phonebook/src/App.tsx
@@ -5,8 +5,6 @@ import "./index.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import mate_logo_feher from "./kepek/mate-logo-feher.png"
 
-var mateblue = "#005f92";
-
 export default class RicsiVagyok extends React.Component {
 
   state = {
@@ -114,81 +112,78 @@ export default class RicsiVagyok extends React.Component {
     });
 
     return (
-      <div className="keret" style={{background: "#007548"}}>
+      <div className="keret">
         <div className="fejlec">
-          <img src={mate_logo_feher} style={{height: 120}}/>
-          <div className="cim">Telefonkönyv</div>
-        </div>
-        <div className="tartalomKartyak">
-          <div className="kartyak">
-          <div className="kartya kartyaKereses">
-            <div className="kartyaNev kartyaNevKereses"><h2>Keresés</h2></div>
-            <div className="kartyaTartalom kartyaTartalomKereses">
-              <div>
-                <div style={{float: 'left', marginTop: 5, marginRight: 5}}>Név:</div>
-                <InputGroup
-                  value={this.state.textboxValue}
-                  onChange={this.textboxChange}
-                  style={{width: 250}}
-                  placeholder="Keresés név alapján"
-                />
-                <div style={{paddingTop: 10, paddingBottom: 10}}>A megadott szövegre a névjegy minden adatában keresünk (pl. név, telefonszám, szobaszám stb.).</div>
+          <div className="fejlecElem">
+            <img src={mate_logo_feher} style={{height: 120}}/>
+            <div className="cim">Telefonkönyv</div>
+          </div>
+          <div>
+            <div className="kereso">
+              <div className="keresoFejlec">
+                <div className="keresoTartalom">
+                Keresés
+                </div>
               </div>
-              <div>
-              <Button
-                icon={<Icon icon="search" color="#fff"/>}
-                text="Keresés"
-                style={{marginRight: 10, background: mateblue, color: '#fff'}}
-                onClick={this.gombKereses}
-                color={Colors.WHITE}
-              />
-              <Button
-                icon={<Icon icon="delete" color="#fff"/>}
-                text="Mégsem"
-                style={{background: '#bd2b30',
-                color: '#fff',
-                marginRight: 5}}
-                onClick={this.gombMegsem}
-              />
+              <div className="keresoTartalom">
+                <div style={{display: 'flex'}}>
+                  <div style={{marginTop: 4, marginRight: 5, fontWeight: "bold", fontSize: 16}}>Név:</div>
+                  <InputGroup
+                    value={this.state.textboxValue}
+                    onChange={this.textboxChange}
+                    placeholder="Keresés név alapján"
+                  />
+                </div>
+                <div style={{paddingTop: 5, paddingBottom: 5}}>A megadott szövegre a névjegy minden adatában keresünk (pl. név, telefonszám, szobaszám stb.).</div>
+                <Button
+                  icon={<Icon icon="search" color="#fff"/>}
+                  text="Keresés"
+                  style={{marginRight: 10, background: "#007548", color: '#fff'}}
+                  onClick={this.gombKereses}
+                  color={Colors.WHITE}
+                />
               </div>
             </div>
           </div>
-            {filteredEmberek.map(item => (
-              <div className="kartya">
-                <div className="kartyaNev"><h2>{item.nev}</h2></div>
-                <div className="kartyaTartalom">
-                  <table>
-                    <tr>
-                      <td className="kartyaBal">Munkakör:</td>
-                      <td className="kartyaJobb">{item.munkakor}</td>
-                    </tr>
-                    <tr>
-                      <td className="kartyaBal">Szervezeti Egység:</td>
-                      <td className="kartyaJobb">{item.szervezetiEgyseg}</td>
-                    </tr>
-                    <tr>
-                      <td className="kartyaBal">Hely:</td>
-                      <td className="kartyaJobb">{item.hely}</td>
-                    </tr>
-                    <tr>
-                      <td className="kartyaBal">Email:</td>
-                      <td className="kartyaJobb">{item.email}</td>
-                    </tr>
-                    <tr>
-                      <td className="kartyaBal">Telefonszám:</td>
-                      <td className="kartyaJobb">{item.kozvetlenSzam}</td>
-                    </tr>
-                  </table>
+          <div className="fejlecElem">Nyelv</div>
+        </div>
+          <div className="tartalom">
+            <div className="kartyak">
+            
+              {filteredEmberek.map(item => (
+                <div className="kartya">
+                  <div className="kartyaNev">{item.nev}</div>
+                  <div className="kartyaTartalom">
+                    <table>
+                      <tr>
+                        <td className="kartyaBal">Munkakör:</td>
+                        <td className="kartyaJobb">{item.munkakor}</td>
+                      </tr>
+                      <tr>
+                        <td className="kartyaBal">Szervezeti Egység:</td>
+                        <td className="kartyaJobb">{item.szervezetiEgyseg}</td>
+                      </tr>
+                      <tr>
+                        <td className="kartyaBal">Hely:</td>
+                        <td className="kartyaJobb">{item.hely}</td>
+                      </tr>
+                      <tr>
+                        <td className="kartyaBal">Email:</td>
+                        <td className="kartyaJobb">{item.email}</td>
+                      </tr>
+                      <tr>
+                        <td className="kartyaBal">Telefonszám:</td>
+                        <td className="kartyaJobb">{item.kozvetlenSzam}</td>
+                      </tr>
+                    </table>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
-        <div className="lablec">
-          <div className="lablecszoveg">
-            Hibabejelentés: helpdesk@uni-mate.hu
+          <div className="lablec">
+              Hibabejelentés: helpdesk@uni-mate.hu
           </div>
-        </div>
       </div>
     )
   }
