77ef622b2fc12b1e6502adbe5985ad48cdddb0bd
BigRicsoo <big.ricsoo@gmail.com>
2021-03-08 15:33:21 +0100

    2021.03.08. 15:33

--- phonebook/src/App.tsx
+++ phonebook/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { ChangeEvent } from 'react';
 import ReactDOM from "react-dom";
-import {  Button, ButtonGroup, Colors, InputGroup, Icon} from "@blueprintjs/core";
+import {  Button, Card, Colors, InputGroup, Icon} from "@blueprintjs/core";
 
 import "./index.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
@@ -8,11 +8,18 @@ import mate_logo_feher from "./kepek/mate-logo-feher.png";
 import hu_flag from "./kepek/hu_flag.png";
 import us_uk_flag from "./kepek/us_uk_flag.png"
 
+
+
+let checkboxClasses = "checkBox checkedOut";
+var egyszeruNevValue = "";
+var isChecked = false;
+var talalatok = 0;
+
 export default class App extends React.Component {
   state = {
-    black: true,
-    textboxValue: "",
-    filter: "",
+    isAdvanced: false,
+    isCheckedState: false,
+    egyszeruFilter: "",
     emberek: [
       {
         nev: "Nagy Balázs",
@@ -97,51 +104,191 @@ export default class App extends React.Component {
     ]
   }
 
-
-
-
-  textboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({textboxValue: event.target.value})
+  egyszeruTextboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    egyszeruNevValue = event.target.value
   };
 
-  gombKereses = () => {
-    this.setState({filter: this.state.textboxValue})
+  egyszeruKeresesGomb = () => {
+    this.setState({egyszeruFilter: egyszeruNevValue})
+    isChecked = this.state.isCheckedState
+    talalatok = 0
   }
 
-  gombMegsem = () => {
-    this.setState({filter: ""})
-    this.setState({textboxValue: ""})
-  }
-
-  keresesEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+  egyszeruKeresesEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
-      this.gombKereses()
+      this.egyszeruKeresesGomb()
     }
   }
 
-  changeColor(){
-    this.setState({black: !this.state.black})
+  checkboxChange(){
+    this.setState({isCheckedState: !this.state.isCheckedState})
+    console.log(isChecked)
   }
 
-  
+  chooseKeresesPanel() {
+    if (this.state.isAdvanced == false){
+      return(
+        <div className="kereso">
+          <div className="keresoFejlec">
+            <div className="keresoTartalom">
+            Keresés
+            </div>
+          </div>
+          <div className="keresoTartalom">
+            <div style={{display: 'flex'}}>
+              <div style={{marginTop: 4, marginRight: 5, fontWeight: "bold", fontSize: 16}}>Név:</div>
+              <InputGroup
+                onChange={this.egyszeruTextboxChange}
+                onKeyPress={this.egyszeruKeresesEnter}
+              />
+            </div>
+            <div className="reszletesKereses">
+              <button className={checkboxClasses} style={{marginRight: 5, marginTop: 8, float: 'left'}} onClick={this.checkboxChange.bind(this)}>{<Icon icon="small-tick"/>}</button>
+              <div>A megadott szövegre a névjegy minden adatában keresünk (pl. név, telefonszám, szobaszám stb.).</div>
+            </div>
+          
+
+            <Button
+              icon={<Icon icon="search" color="#fff"/>}
+              text="Keresés"
+              style={{marginRight: 10, background: "#007548", color: '#fff'}}
+              onClick={this.egyszeruKeresesGomb}
+              color={Colors.WHITE}
+            />
+            <Button
+              icon={<Icon icon="settings" color="#fff"/>}
+              text="Részletes Keresés"
+              style={{marginRight: 10, background: "#007548", color: '#fff'}}
+              onClick={()=> this.setState({isAdvanced: true})}
+              color={Colors.WHITE}
+            />
+            </div>
+          </div>
+        )}
+    else {
+      return(
+        <div className="reszletesKerso">
+          <div className="keresoFejlec">
+            <div className="keresoTartalom">
+              Részletes Keresés
+            </div>
+          </div>
+          <div className="keresoTartalom">
+            <div style={{paddingBottom: 5}}>
+              <table>
+                <tr>
+                  <td className="kartyaBal"><div className="reszletesBal">Név:</div></td>
+                  <td className="kartyaJobb"> 
+                    <InputGroup
+                    value={egyszeruNevValue}
+                    onChange={this.egyszeruTextboxChange}
+                    onKeyPress={this.egyszeruKeresesEnter}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="kartyaBal"><div className="reszletesBal">  Szervezeti egység:</div></td>
+                  <td className="kartyaJobb">
+                    <InputGroup
+                    value={egyszeruNevValue}
+                    onChange={this.egyszeruTextboxChange}
+                    onKeyPress={this.egyszeruKeresesEnter}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="kartyaBal"><div className="reszletesBal">Munkakör:</div></td>
+                  <td className="kartyaJobb">
+                    <InputGroup
+                    value={egyszeruNevValue}
+                    onChange={this.egyszeruTextboxChange}
+                    onKeyPress={this.egyszeruKeresesEnter}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="kartyaBal"><div className="reszletesBal">Hely:</div></td>
+                  <td className="kartyaJobb">
+                    <InputGroup
+                    value={egyszeruNevValue}
+                    onChange={this.egyszeruTextboxChange}
+                    onKeyPress={this.egyszeruKeresesEnter}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="kartyaBal"><div className="reszletesBal">Email:</div></td>
+                  <td className="kartyaJobb">
+                    <InputGroup
+                    value={egyszeruNevValue}
+                    onChange={this.egyszeruTextboxChange}
+                    onKeyPress={this.egyszeruKeresesEnter}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="kartyaBal"><div className="reszletesBal">Telefon:</div></td>
+                  <td className="kartyaJobb">
+                    <InputGroup
+                    value={egyszeruNevValue}
+                    onChange={this.egyszeruTextboxChange}
+                    onKeyPress={this.egyszeruKeresesEnter}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="kartyaBal"><div className="reszletesBal">Telefon Mellék:</div></td>
+                  <td className="kartyaJobb">
+                    <InputGroup
+                    value={egyszeruNevValue}
+                    onChange={this.egyszeruTextboxChange}
+                    onKeyPress={this.egyszeruKeresesEnter}
+                    />
+                  </td>
+                </tr>
+              </table>
+            </div>
+              <Button
+              icon={<Icon icon="search" color="#fff"/>}
+              text="Keresés"
+              style={{marginRight: 10, background: "#007548", color: '#fff'}}
+              onClick={this.egyszeruKeresesGomb}
+              color={Colors.WHITE}
+            />
+            <Button
+              icon={<Icon icon="undo" color="#fff"/>}
+              text="Vissza"
+              style={{marginRight: 10, background: "#007548", color: '#fff'}}
+              onClick={()=> this.setState({isAdvanced: false})}
+              color={Colors.WHITE}
+            />
+            </div>
+          </div>
+    )}
+  }
 
   render() {
-    const { filter, emberek } = this.state;
-    const lowercasedFilter = filter.toLowerCase();
+    const { egyszeruFilter, emberek } = this.state;
+    const lowercasedFilter = egyszeruFilter.toLowerCase();
     const filteredEmberek = emberek.filter(item => {
       if (lowercasedFilter != ""){
-        return (
-          item.nev.toLowerCase().includes(lowercasedFilter) ||
-          item.email.toLowerCase().includes(lowercasedFilter) ||
-          item.hely.toLowerCase().includes(lowercasedFilter) ||
-          item.kozvetlenSzam.toLowerCase().includes(lowercasedFilter) ||
-          item.munkakor.toLowerCase().includes(lowercasedFilter) ||
-          item.szervezetiEgyseg.toLowerCase().includes(lowercasedFilter)
-        )
+        if (isChecked == true){
+          return (
+            item.nev.toLowerCase().includes(lowercasedFilter) ||
+            item.email.toLowerCase().includes(lowercasedFilter) ||
+            item.hely.toLowerCase().includes(lowercasedFilter) ||
+            item.kozvetlenSzam.toLowerCase().includes(lowercasedFilter) ||
+            item.munkakor.toLowerCase().includes(lowercasedFilter) ||
+            item.szervezetiEgyseg.toLowerCase().includes(lowercasedFilter)
+          )
+        }
+        else{
+          return (item.nev.toLowerCase().includes(lowercasedFilter))
+        }
       }
     });
 
-    let checkBox = this.state.black ? "checkBox checkedIn" : "checkBox checkedOut";
+    checkboxClasses = this.state.isCheckedState ? "checkBox checkedOut" : "checkBox checkedIn";
 
     return (
       <div className="keret">
@@ -151,52 +298,22 @@ export default class App extends React.Component {
             <div className="cim">Telefonkönyv</div>
           </div>
           <div>
-            <div className="kereso">
-              <div className="keresoFejlec">
-                <div className="keresoTartalom">
-                Keresés
-                </div>
-              </div>
-              <div className="keresoTartalom">
-                <div style={{display: 'flex'}}>
-                  <div style={{marginTop: 4, marginRight: 5, fontWeight: "bold", fontSize: 16}}>Név:</div>
-                  <InputGroup
-                    value={this.state.textboxValue}
-                    onChange={this.textboxChange}
-                    placeholder="Keresés név alapján"
-                    onKeyPress={this.keresesEnter}
-                  />
-                </div>
-                <div className="reszletesKereses">
-                  <button className={checkBox} style={{marginRight: 5, marginTop: 8, float: 'left'}} onClick={this.changeColor.bind(this)}>{<Icon icon="small-tick"/>}</button>
-                  <div>A megadott szövegre a névjegy minden adatában keresünk (pl. név, telefonszám, szobaszám stb.).</div>
-                </div>
-
-                <Button
-                  icon={<Icon icon="search" color="#fff"/>}
-                  text="Keresés"
-                  style={{marginRight: 10, background: "#007548", color: '#fff'}}
-                  onClick={this.gombKereses}
-                  color={Colors.WHITE}
-                />
-                <Button
-                  icon={<Icon icon="settings" color="#fff"/>}
-                  text="Részletes Keresés"
-                  style={{marginRight: 10, background: "#007548", color: '#fff'}}
-                  onClick={this.gombKereses}
-                  color={Colors.WHITE}
-                />
-              </div>
-            </div>
+            {this.chooseKeresesPanel()}
+            
           </div>
           <div className="fejlecElem">
-              <img src={hu_flag} className="zaszlo"/>
-              <img src={us_uk_flag} className="zaszlo"/>
-            </div>
+            <Card style={{float: 'right', padding: 5}}>
+              <div style={{height: 40}}>
+                <img src={hu_flag} className="zaszlo hu"/>
+                <img src={us_uk_flag} className="zaszlo eng"/>
+              </div>
+            </Card>
+          </div>
         </div>
           <div className="tartalom">
+            <div>Találatok száma: {talalatok}</div>
             <div className="kartyak">
-              {filteredEmberek.map(item => (
+              {filteredEmberek.map(item => ( 
                 <div className="kartya">
                   <div className="kartyaNev">{item.nev}</div>
                   <div className="kartyaTartalom">
