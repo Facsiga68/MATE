6920d078a35dc12b9cc0c282fcce868df4154000
bogdanytamas <bogdanytamas377@gmail.com>
2021-03-17 14:28:52 +0100

    login1.0

--- joker/src/App.tsx
+++ joker/src/App.tsx
@@ -1,95 +1,88 @@
-import React,  { useState } from 'react';
+import React from 'react';
+import Axios from "axios";
 import './App.css';
-import LoginForm from './components/LoginForm';
+import { Button, InputGroup, Icon } from "@blueprintjs/core";
+//import { Tooltip2 } from "@blueprintjs/popover2";
+import crypto from 'crypto';
+import CSS from 'csstype';
 
-import "@blueprintjs/core/lib/css/blueprint.css";
-import { Button, ButtonGroup, FormGroup, InputGroup } from '@blueprintjs/core';
-import { Column, Table, Cell } from "@blueprintjs/table";
-import "@blueprintjs/table/lib/css/table.css";
-import { render } from '@testing-library/react';
+import './login.css';
+import logo from './images/logo.png';
 
-function App() {
-  const adminUser = {
-    azonosito: "a",
-    jelszo: "a"
-  }
-
-  const adminTomi = {
-    azonosito: "tomi",
-    jelszo: "kiraly"
-  }
-
-  const cellRenderer = (rowIndex: number) => {
-    return <Cell>{"aaa"}</Cell>
+export default class App extends React.Component {
+  
+  state = {
+    fnev: '',
+    fjelszo: '',
+    lekerNev: '',
+    rendszegazda: false,
+    keresNev: ''
   };
 
-  const users = [
-    {nev: "Tamas", kor: "25", hely: "Gyongyos"},
-    {nev: "David", kor: "20", hely: "Matraszolos"},
-    {nev: "Richard", kor: "22", hely: "Gyongyos"}
-  ]
-
-  const [user, setUser] = useState({ azonosito: ""});
-  const [error, setError] = useState("");
-
-  const Login = (details: any) => {
-    console.log(details);
-
-    if (details.azonosito == adminUser.azonosito && details.jelszo == adminUser.jelszo || details.azonosito == adminTomi.azonosito && details.jelszo == adminTomi.jelszo)
-    {
-      console.log("Bejelentkezve!");
-      setUser({
-        azonosito: details.azonosito
-      })
-    }
-    else
-    {
-      console.log("Nem egyezik az adat!");
-      setError("Nem egyezik az adat!");
-    }
+  private BelepesTeszt = async () => {
+      let FNEV=this.state.fnev;
+      let FJELSZO=crypto.createHash('sha256').update(this.state.fjelszo).digest('base64');
+      let result = await Axios({
+        method: "POST", 
+        url: process.env.REACT_APP_SERVER_URL+"joker/MATEazonositas",
+        data: {FNEV: FNEV, FJELSZO: FJELSZO},
+        withCredentials: true
+      });
+      console.log(result.data);
+      if (result.data.BELEPESEGYEZES == true)
+      {
+        alert("Sikeres bejelentkezés!");
+      }
+      else
+      {
+        alert("Sikertelen bejelentkezés!");
+      }
   }
 
-  const Logout = () => {
-    setUser({ azonosito: ""});
+  fNevValtozik = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({fnev: event.target.value})
+  };
+
+  fJelszoValtozik = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({fjelszo: event.target.value})
+  };
+
+  RGazda = () => {
+    this.setState({rendszegazda: !this.state.rendszegazda})
   }
 
-  return (
-    
-    <div className="App">
-      {(user.azonosito != "") ? (
-      <div className="fooldal" >
-        <h2>Sikeres bejelentkezés!</h2>
+  render() {
 
-        <div className="fejlec">
-          <ButtonGroup>
-            <Button text="Fiók adatok"></Button>
-            <Button text="Szolgáltatások"></Button>
-            <Button text="Jelszó változtatás"></Button>
-            <Button onClick={Logout}>Kilépés</Button>
-          </ButtonGroup>
+    let stilus: CSS.Properties = {
+      marginLeft: "20px"
+    }
+    let checkBox = this.state.rendszegazda ? "checkBox checkedIn" : "checkBox checkedOut";
+    return (
+      <div className="page">
+
+        <div className="header">
+        <h1>IT Jogosultságkezelő Rendszer</h1>
+        </div>
+ 
+        <div className="content">
+          <div className="logo">
+            <img src={logo}/>
+          </div>
+          <div className="login">
+            <InputGroup onChange={this.fNevValtozik} placeholder="Felhasználói név" style={{width: "200px", height: "30px", border: "solid", borderWidth: "thin", marginBottom: "5px"}} />
+            <InputGroup onChange={this.fJelszoValtozik} placeholder="Jelszó" style={{width: "200px", height: "30px", border: "solid", borderWidth: "thin"}} />
+            <Button intent="success" text="Belépés" onClick={this.BelepesTeszt} style={{height: "25px", width: "200px", margin: "20px", backgroundColor: "seagreen", color: "white"}}/>
+            <p style={{color: "#D52222", maxWidth: "500px", textAlign: "center", margin: "20px"}}>Munkavállalók esetében a MATE azonosító 3 betű 4 szám alakú, Hallgatók esetében az azonosító egyezik a NEPTUN kóddal.</p>
+            <p style={{maxWidth: "500px", textAlign: "center"}}>Ha korábban már használta a webmail, wifi, vagy elearning rendszereket, akkor az ezen rendszerekben használt jelszóval kísérelje meg itt is a bejelentkezést, ne az alapértelmeztt jelszóval!</p>
+          </div>
         </div>
 
-        <div className="tartalom">
-    
-          <Table numRows={10}>
-            <Column name="megnevezes" cellRenderer={cellRenderer}/>
-            <Column name="adat"/>
-          </Table>
-
-          <Table numRows={3}>
-            <Column name="megnevezes"/>
-            <Column name="adat" cellRenderer={cellRenderer}/>
-            <Column/>
-          </Table>
-
+        <div className="footer">
+          <p style={{textAlign: "center"}}>Hibabejelentés: helpdesk@uni-mate.hu</p>
         </div>
-
-      </div>
-     ) : (
-      <LoginForm Login={Login} error={error}/>
-    )}
-    </div>
-  );
+        
+      </div> 
+    );
+  
+  }
 }
-
-export default App;
